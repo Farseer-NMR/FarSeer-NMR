a2a18584e84a11c02b1dfbd4e5b23419c53cd30c
João Miguel Correia Teixeira <joaomcteixeira@gmail.com>
2017-04-28 16:35:40 +0200

    Added comments to identify the menus

--- current/farseer_user_variables.py
+++ current/farseer_user_variables.py
@@ -1,36 +1,39 @@
 import sys
-#
+### Paths to somewhere:
 spectra_path = sys.argv[1]  # where spectra peaklists .csv files are stored - relative path
-#
 logfile_name = 'file.log' #  the name of the log file
-#
-has_sidechains = True  # Are there sidechain entries in the peaklists?
-use_sidechains = True  # Do you want to analyse those sidechains?
+####################################
+
+### Input Data Setup
+applyFASTA = True  # complete the sequence with a FASTA file?
+FASTAstart = 1  # Residue number for the first residue in the FASTA file?
 #
 perform_cs_correction = False  # Aligns peaklists to a specific residue in the reference spectra
 cs_correction_res_ref = 76  # To which residue?
 #
+has_sidechains = True  # Are there sidechain entries in the peaklists?
+use_sidechains = True  # Do you want to analyse those sidechains?
+#
 expand_lost_yy = False  # Considers lost residue over yy references and xx reference
 expand_lost_zz = False  # Considers lost residue over zz references and xx reference
-#
-applyFASTA = True  # complete the sequence with a FASTA file?
-FASTAstart = 1  # Residue number for the first residue in the FASTA file?
-#
+####################################
+
+### Calculations Setup
 do_cond1 = True  # Analyse data in the first titration condition (dimension)
 do_cond2 = False  # Analyse data in the second titration condition (dimension)
 do_cond3 = False  # Analyse data in the thrid titration condition (dimension)
+#
 perform_comparisons = False  # Compares data obtained for condition experiment.
 #
 csp_alpha4res = 0.14  # General alpha normalization factor for CSP
 csp_res_exceptions = {'G': 0.2}  # exceptions for the normalization factor
 cs_lost = 'prev'  # how to represent the lost residues in CSPs [prev/full]
 #
-chimera_att_select_format = ':'  # format to select residues in Chimera
-#
 perform_resevo_fit = False  # Fit parameter evolution over titration experiment
+#
 titration_x_values = [0,12.5, 25, 37.5, 50, 75, 100, 150, 300] # values for the x axis in the fitting procedure (ex. Ligand concentration)
-
-# Perform PRE analysis
+#
+## Perform PRE analysis
 apply_PRE_analysis = False
 apply_smooth=True
 gaussian_stddev=1
@@ -40,40 +43,52 @@ pre_lw=1  # theoretical PRE line width
 tag_color='blue'
 tag_lw=0.8
 tag_ls='-'
-
+#####################################
 #
+#
+### Data Representation Setup
+#
+chimera_att_select_format = ':'  # format to select residues in Chimera
+#
+# Restrains to Plot:
 plots_PosF1_delta = True # Plot nuclei 1 shift perturbation data
 plots_PosF2_delta = False # Plot nuclei 2 shift perturbation data
 plots_CSP = False  # Plot combined chemical shift perturbation data
 plots_Height_ratio = False  # Plot Height ratio data
 plots_Volume_ratio = False  # Plot Volume ratio data
 #
+# Ploting Templates
 plots_extended_bar = True  # Represent data in Extended Bar Plot style
 plots_compacted_bar = True  # Represent data in Compacted Bar Plot style
 plots_vertical_bar = True # Represent data in Vertical Bar Plot style
 plots_residue_evolution = True  # Represent Data Evolution per Residue style
 plots_cs_scatter = True  # Represent chemical shift scatter data
 #
+# Y Axis Label for each restraint
 yy_label_PosF1_delta = 'ppm'  # y axis label for nuclei 1
 yy_label_PosF2_delta = 'ppm'  # y axis label for nuclei 2
 yy_label_CSP = 'CSPs (ppm)'  # y axis label for combined chemical shift
 yy_label_Height_ratio = 'Hi/H0'  # y axis label for height ratio
 yy_label_Volume_ratio = 'Vi/V0'  # y axis label for volume ratio
 #
+# Restraint column name for the output
 calccol_name_PosF1_delta = 'H1_delta'  # column name for nuclei 1
 calccol_name_PosF2_delta = 'N15_delta'  # column name for nuclei 2
 calccol_name_CSP = 'CSP'  # column name for combined chemical shift perturbation data
 calccol_name_Height_ratio = 'Height_ratio'  # column name for Height Ratio data
 calccol_name_Volume_ratio = 'Vol_ratio'  # column name for Volume Ratio data
 #
+# Y axis scale for each calculated restraint
 yy_scale_PosF1_delta = 0.1  # y axis sacle for nuclei 1
 yy_scale_PosF2_delta = 0.25  # y axis scale for nuclei 2
 yy_scale_CSP = 0.3  # y axis sacle for combined chemical shift
 yy_scale_Height_ratio = 1.5 # y axis scale for height ratio
 yy_scale_Volume_ratio = 1.5 # y axis scale for volume ratio
 yy_scale_nbins=5  # adjusted number of ticks
+#####################################
 #
-# General Titration Plots Variables
+#### Plot configuration variables
+### General Titration Plots Variables
 tplot_subtitle_fn= 'Arial'  # font type for the subplot titles.
 tplot_subtitle_fs= 8  # font size for the subplot titles.
 tplot_subtitle_pad= 0.99  # subplot title separation from y
@@ -100,7 +115,8 @@ tplot_y_grid_linestyle='-'  # style of grid line
 tplot_y_grid_linewidth=0.2  # grid line width
 tplot_y_grid_alpha=0.8  # grid transparency
 tplot_vspace=0.5 # vertical spacing between plots
-# General Bar Plots Variables
+#
+## General Bar Plots Variables
 bar_measured_color= 'black'  # bar color for measured peaks
 bar_status_color_flag= True  # applies color to the 'lost' residues
 bar_lost_color= 'red'  # bar color for the lost residues
@@ -166,7 +182,31 @@ comp_bar_unassigned_shade_alpha=0.5  # unassigned residues shade transparency.
 vert_bar_cols_page = 5  # number of columns of subplots per page
 vert_bar_rows_page = 2  # number of rows of subplots per page
 #
-# General Variables for Evolution per Residue
+# Specific details for DELTA PRE oscilations Plot
+dpre_osci_rows=10
+dpre_osci_width=3  # scale factor for the width of the plot
+dpre_osci_y_label = r'$\Delta$PRE$_(rc-exp)$'
+dpre_osci_y_label_fs=5
+dpre_osci_dpre_ms=2  # DELTA_PRE circle marker size
+dpre_osci_ymax=0.8
+dpre_osci_dpre_alpha=0.5  # DELTA_PRE alpha
+dpre_osci_smooth_lw=1  # smooted DPRE line width
+dpre_osci_ref_color='black'  # color for the reference data
+dpre_osci_color_init='#ff00ff'  # initial color for data points color gradient
+dpre_osci_color_end='#0000ff'  # final color for data points color gradient
+dpre_osci_x_ticks_fn='Arial'  # xticks font
+dpre_osci_x_ticks_fs=5  # x ticks font size
+dpre_osci_x_ticks_pad = 0.5
+dpre_osci_x_ticks_weight = 'normal'
+dpre_osci_grid_color='grey'  # grid color
+dpre_osci_shade = False  # applies shade to regions
+dpre_osci_shade_regions = [(23,37),(0,0),(0,0)]  # defines shade regions
+dpre_osci_res_highlight=True  # flags residue highlight
+dpre_osci_res_hl_list=[25,32,54,64,66,47]  # residues to highlight (res, shade width)
+dpre_osci_rh_fs=4  # font size for residue highlight
+dpre_osci_rh_y=0.9  # y position for residue 1-letter representation
+#
+## General Variables for Evolution per Residue
 revo_subtitle_fn='Arial'  # subplot title font
 revo_subtitle_fs=8  # subplot title font size
 revo_subtitle_pad=0.98  # subplot title pad
@@ -189,7 +229,7 @@ revo_y_ticks_fs=5  # x ticks font size
 revo_y_ticks_pad=1  # x ticks pad
 revo_y_ticks_weight=1  # x ticks font style
 revo_y_ticks_rot=0
-
+#
 # Specific details for Residue Evolution Plot
 # use a combination of cols_page and rows_page to achieve desired figure ratio
 res_evo_cols_page = 5  # number of columns of subplots per page
@@ -225,7 +265,7 @@ cs_scatter_mk_color='none'  # marker inside color for shape style
 cs_scatter_mk_edgecolors='black'  # marker edge color for shape style
 cs_scatter_mk_lost_color='red'  # color for lost data points - color style
 #
-# DELTA PRE Heat Maps
+### DELTA PRE Heat Maps
 heat_map_rows = 20
 heat_map_vmin=0.2
 heat_map_vmax=0.7
@@ -242,32 +282,10 @@ heat_map_right_margin=0.2
 heat_map_bottom_margin=0.5
 heat_map_top_margin=0.9
 heat_map_cbar_font_size=4
+########################################
+
 #
-# DELTA PRE oscilations Plot
-dpre_osci_rows=10
-dpre_osci_width=3  # scale factor for the width of the plot
-dpre_osci_y_label = r'$\Delta$PRE$_(rc-exp)$'
-dpre_osci_y_label_fs=5
-dpre_osci_dpre_ms=2  # DELTA_PRE circle marker size
-dpre_osci_ymax=0.8
-dpre_osci_dpre_alpha=0.5  # DELTA_PRE alpha
-dpre_osci_smooth_lw=1  # smooted DPRE line width
-dpre_osci_ref_color='black'  # color for the reference data
-dpre_osci_color_init='#ff00ff'  # initial color for data points color gradient
-dpre_osci_color_end='#0000ff'  # final color for data points color gradient
-dpre_osci_x_ticks_fn='Arial'  # xticks font
-dpre_osci_x_ticks_fs=5  # x ticks font size
-dpre_osci_x_ticks_pad = 0.5
-dpre_osci_x_ticks_weight = 'normal'
-dpre_osci_grid_color='grey'  # grid color
-dpre_osci_shade = False  # applies shade to regions
-dpre_osci_shade_regions = [(23,37),(0,0),(0,0)]  # defines shade regions
-dpre_osci_res_highlight=True  # flags residue highlight
-dpre_osci_res_hl_list=[25,32,54,64,66,47]  # residues to highlight (res, shade width)
-dpre_osci_rh_fs=4  # font size for residue highlight
-dpre_osci_rh_y=0.9  # y position for residue 1-letter representation
-#
-# Figure Details
+### Figure Details
 fig_width = 8.69  # Figure width in inches
 fig_height = 11.69  # Figure height in inches
 fig_file_type = 'pdf'  # Figure file type
