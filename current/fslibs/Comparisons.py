65010e5a4315f0e5744071c1b93f434dbdefad4e
joaomcteixeira <joaomcteixeira@gmail.com>
2017-08-21 11:07:15 +0200

    commented comparisons

--- current/fslibs/Comparisons.py
+++ current/fslibs/Comparisons.py
@@ -3,8 +3,53 @@ import pandas as pd
 
 class Comparisons:
     """
-    This class stores the Titration data of all the titrations
-    and parsed it so that results are compared accross titrations.
+    Prepares parsed data along next and previous axis of the calculation.
+    
+    Given a dictionary containing all the series (FarseerSeries) along
+    a given axis of the Farseer-NMR Cube, the Comparisons class stores
+    that dictionary and parses it along the two other axis (next and prev)
+    so that, for example, data generated along X can be compared along
+    Y and Z, where Y is the next axis of X and Z and previous axis of X.
+    
+    Different dictionaries are stored for next and previous parsed axes.
+    
+    Args:
+        dimension (str): identifies the main dimension axis of the class
+            where X = cond1, Y = cond2, Z = cond3
+        
+        hyper_panel (5-dimension pandas.Panel): converted from dictionary     
+            stores all the main axis series.
+        
+        other_dim_keys (lst): ordered list containing the previous and next
+            dimension names, same nomenclature as 'dimension'.
+        
+        all_next_dim (dict): stores all the series of parsed data along
+            the next dimension. Series are parsed from hyper_panel.
+        
+        all_prev_dim (dict): same as all_next_dim but for the previous axis.
+        
+        has_points_next_dim (bool): True if points are found along the
+            next dimension. False by default.
+        
+        has_points_prev_dim (bool): True if points are found along the
+            previous dimension. False by default.
+        
+        log (str): stores the whole log.
+        
+        log_export_onthefly (bool): Flag that activates on-the-fly log
+            on an external file.
+        
+        log_export_name (str): the name of the external log file that is
+            written on-the-fly.
+    
+    Methods:
+        .log_r()
+        .exports_log()
+        .abort()
+        .gen_next_dim()
+        .gen_prev_dim()
+        .transfer_log()
+        
     """
     def __init__(self,
                  dimension_dict,
@@ -12,8 +57,8 @@ class Comparisons:
                  other_dim_keys=['condy','condz'],
                  resonance_type='Backbone'):
         """
-        :dimension_dict: is a dictionary containing all the titrations
-                         for that dimension.
+        dimension_dict (dict): is a dictionary containing all the series
+            for the main dimension.
         """
         self.p5d = pd.core.panelnd.create_nd_panel_factory(\
             klass_name='Panel5D',
@@ -30,8 +75,6 @@ class Comparisons:
         # performed
         self.dimension = selfdim
         
-        # generates a p5d with all the titrations passed
-        # 
         self.hyper_panel = self.p5d(dimension_dict)
         
         
@@ -64,7 +107,7 @@ class Comparisons:
         Registers the log and prints to the user.
         
         :logstr: the string to be registered in the log
-        :istitle: flag to format logstr as a title
+        :istitle: is True, formats logstr as a title
         """
         if istitle:
             logstr = """
@@ -84,7 +127,7 @@ class Comparisons:
                 logfile.write(logstr)
         return
     
-    def exports_log(self, mod='w', logfile_name='FarseerSet_log.md'):
+    def exports_log(self, mod='w', logfile_name='Comparison_log.md'):
         """ Exports log to external file. """
         with open(logfile_name, mod) as logfile:
             logfile.write(self.log)
@@ -98,8 +141,16 @@ class Comparisons:
     def gen_next_dim(self, series_class, comp_kwargs):
         """
         Generates dictionary with the Series parsed along the next dimension
-        of the <self.selfdim>. Series are of class <series_class>, usually
-        fss.FarseerSeries.
+        of the <self.dimension>.
+        
+        series_class (class): fss.FarseerSeries.
+        comp_kwargs (dict): contains kwargs to initiate FarseerSeries.
+        
+        Returns:
+            None.
+            
+        Modifies Arg:
+            all_next_dim (dict)
         """
         
         self.log_r(\
@@ -147,9 +198,17 @@ class Comparisons:
     
     def gen_prev_dim(self, series_class, comp_kwargs):
         """
-        Generates dictionary with the Series parsed along the previrous
-        dimension of the <self.selfdim>. Series are of class <series_class>, 
-        usually fss.FarseerSeries.
+        Generates dictionary with the Series parsed along the previous
+        dimension of the <self.dimension>.
+        
+        series_class (class): fss.FarseerSeries.
+        comp_kwargs (dict): contains kwargs to initiate FarseerSeries.
+        
+        Returns:
+            None.
+            
+        Modifies Arg:
+            all_prev_dim (dict)
         """
         
         self.log_r(\
@@ -195,7 +254,8 @@ class Comparisons:
     
     def transfer_log(self):
         """
-        Transfers logs from the comparison Series to the main class object.
+        Transfers logs from the Series objects in self.all_prev_dim
+        and self.all_next_dim to the main class object.
         """
         if self.has_points_next_dim:
             for dim2_pt in sorted(self.all_next_dim.keys()):
