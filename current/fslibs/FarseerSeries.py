fb9a24281be6adec01fe56a369248fc0c2e9d638
joaomcteixeira <joaomcteixeira@gmail.com>
2017-12-14 17:41:08 +0100

    added copyright notice from Ben Southgate for color gradient functions

--- current/fslibs/FarseerSeries.py
+++ current/fslibs/FarseerSeries.py
@@ -261,7 +261,32 @@ class FarseerSeries(pd.Panel):
         return
     
     def hex_to_RGB(self, hexx):
-        """http://bsou.io/posts/color-gradients-with-python
+        """
+        This function was taken verbatin from:
+        Copyright 2017 Ben Southgate
+        https://github.com/bsouthga/blog
+        
+        The MIT License (MIT)
+        
+        Permission is hereby granted, free of charge,
+        to any person obtaining a copy of this software and associated
+        documentation files (the "Software"), to deal in the Software without
+        restriction, including without limitation the rights to use, copy,
+        modify, merge, publish, distribute, sublicense, and/or sell copies of
+        the Software, and to permit persons to whom the Software is furnished
+        to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
         "#FFFFFF" -> [255,255,255]
         """
         # Pass 16 to the integer function for change of base
@@ -269,7 +294,31 @@ class FarseerSeries(pd.Panel):
 
     def RGB_to_hex(self, RGB):
         """
-        http://bsou.io/posts/color-gradients-with-python
+        This function was taken verbatin from:
+        Copyright 2017 Ben Southgate
+        https://github.com/bsouthga/blog
+        
+        The MIT License (MIT)
+        
+        Permission is hereby granted, free of charge,
+        to any person obtaining a copy of this software and associated
+        documentation files (the "Software"), to deal in the Software without
+        restriction, including without limitation the rights to use, copy,
+        modify, merge, publish, distribute, sublicense, and/or sell copies of
+        the Software, and to permit persons to whom the Software is furnished
+        to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        
         [255,255,255] -> "#FFFFFF"
         """
         # Components need to be integers for hex to make sense
@@ -278,7 +327,31 @@ class FarseerSeries(pd.Panel):
     
     def color_dict(self, gradient):
         """
-        http://bsou.io/posts/color-gradients-with-python
+        This function was taken verbatin from:
+        Copyright 2017 Ben Southgate
+        https://github.com/bsouthga/blog
+        
+        The MIT License (MIT)
+        
+        Permission is hereby granted, free of charge,
+        to any person obtaining a copy of this software and associated
+        documentation files (the "Software"), to deal in the Software without
+        restriction, including without limitation the rights to use, copy,
+        modify, merge, publish, distribute, sublicense, and/or sell copies of
+        the Software, and to permit persons to whom the Software is furnished
+        to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        
         Takes in a list of RGB sub-lists and returns dictionary of
         colors in RGB and hex form for use in a graphing function
         defined later on.
@@ -291,7 +364,31 @@ class FarseerSeries(pd.Panel):
     
     def linear_gradient(self, start_hex, finish_hex="#FFFFFF", n=10):
         """
-        http://bsou.io/posts/color-gradients-with-python
+        This function was taken verbatin from:
+        Copyright 2017 Ben Southgate
+        https://github.com/bsouthga/blog
+        
+        The MIT License (MIT)
+        
+        Permission is hereby granted, free of charge,
+        to any person obtaining a copy of this software and associated
+        documentation files (the "Software"), to deal in the Software without
+        restriction, including without limitation the rights to use, copy,
+        modify, merge, publish, distribute, sublicense, and/or sell copies of
+        the Software, and to permit persons to whom the Software is furnished
+        to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        
         returns a gradient list of (n) colors between
         two hex colors. start_hex and finish_hex
         should be the full six-digit color string,
