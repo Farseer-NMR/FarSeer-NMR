714036c4912333a19aa5316e69917d15192245b0
joaomcteixeira <joaomcteixeira@gmail.com>
2018-02-09 17:58:03 +0100

    removed minor new_line symbols hanging

--- gui/components/TabWidget.py
+++ gui/components/TabWidget.py
@@ -9,7 +9,7 @@ This file is part of Farseer-NMR.
 
 Farseer-NMR is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or¶
+the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 Farseer-NMR is distributed in the hope that it will be useful,
@@ -18,7 +18,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Farseer-NMR. If not, see <http://www.gnu.org/licenses/>.¡
+along with Farseer-NMR. If not, see <http://www.gnu.org/licenses/>.
 """
 import os
 
