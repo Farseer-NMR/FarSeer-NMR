8461becfa67cb82b86b2615ac5864256451528a1
joaomcteixeira <joaomcteixeira@gmail.com>
2018-02-20 13:19:48 +0100

    main

--- main.py
+++ main.py
@@ -38,60 +38,54 @@ from core.fslibs.Variables import Variables
 class Main(QWidget):
 
     def __init__(self, parent=None, gui_settings=None, config=None, **kw):
-
         QWidget.__init__(self, parent=parent)
-
-        default_config = os.path.join(os.path.dirname(os.path.abspath(
-            __file__)), 'core',
-            'default_config.json')
-
+        default_config = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            'core',
+            'default_config.json'
+            )
+        #
         if config:
             Variables().read(config)
         else:
             Variables().read(default_config)
-
+        #
         tabWidget = TabWidget(gui_settings)
-
         footer = Footer(self, gui_settings=gui_settings)
-
         layout = QVBoxLayout(self)
         self.setLayout(layout)
         self.layout().setAlignment(QtCore.Qt.AlignTop)
         self.layout().addWidget(tabWidget)
         self.layout().addWidget(footer)
-
         self.setObjectName("MainWidget")
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     import argparse
-
+    
     parser = argparse.ArgumentParser(description='Run Farseer')
-    parser.add_argument('--config', metavar='path', required=False,
-                        help='Farseer Configuration File')
+    parser.add_argument(
+        '--config',
+        metavar='path',
+        required=False,
+        help='Farseer Configuration File'
+        )
     splash_pix = QtGui.QPixmap('gui/images/splash-screen.png')
-
     args = parser.parse_args()
-
     splash = QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
-    splash.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint
-                          | QtCore.Qt.FramelessWindowHint)
+    splash.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
     splash.setEnabled(False)
     splash.show()
-
     screen_resolution = app.desktop().screenGeometry()
-
+    
     from gui import gui_utils
     gui_settings, stylesheet = gui_utils.deliver_settings(screen_resolution)
-
+    
     ex = Main(gui_settings=gui_settings, config=args.config)
     splash.finish(ex)
     font_file = 'gui/SinkinSans/SinkinSans-400Regular.otf'
     font_id = QtGui.QFontDatabase.addApplicationFont(font_file)
-
     app.setStyleSheet(stylesheet)
-
     ex.show()
     ex.raise_()
     sys.exit(app.exec_())
