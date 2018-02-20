7d2e3d741656d79d642f315ab6845962b00dc3fb
joaomcteixeira <joaomcteixeira@gmail.com>
2018-02-20 10:58:46 +0100

    pre analysis

--- gui/popups/PreAnalysisPopup.py
+++ gui/popups/PreAnalysisPopup.py
@@ -23,7 +23,6 @@ along with Farseer-NMR. If not, see <http://www.gnu.org/licenses/>.
 from PyQt5.QtWidgets import QDialogButtonBox
 
 from gui.components.LabelledSpinBox import LabelledSpinBox
-
 from gui.popups.BasePopup import BasePopup
 
 
@@ -41,38 +40,20 @@ class PreAnalysisPopup(BasePopup):
         .set_values()
     """
     def __init__(self, parent=None, **kw):
-        BasePopup.__init__(self, parent, title="PRE Settings",
-                           settings_key=["pre_settings"])
-
-        self.gaussian_stdev = LabelledSpinBox(
-                                              self,
-                                              "Gaussian Stdev",
-                                              minimum=1,
-                                              step=1
-                                              )
-        self.gauss_x_size = LabelledSpinBox(
-                                            self,
-                                            "Gaussian X Size",
-                                            minimum=1,
-                                            step=1
-                                            )
-
+        BasePopup.__init__(self, parent, title="PRE Settings", settings_key=["pre_settings"])
+        self.gaussian_stdev = LabelledSpinBox(self, "Gaussian Stdev", minimum=1, step=1)
+        self.gauss_x_size = LabelledSpinBox(self, "Gaussian X Size", minimum=1, step=1)
         self.layout().addWidget(self.gauss_x_size, 0, 0)
         self.layout().addWidget(self.gaussian_stdev, 1, 0)
-
         self.buttonBox = QDialogButtonBox(
-                                          QDialogButtonBox.Ok |
-                                          QDialogButtonBox.Cancel |
-                                          QDialogButtonBox.RestoreDefaults
-                                          )
-
+            QDialogButtonBox.Ok |
+            QDialogButtonBox.Cancel |
+            QDialogButtonBox.RestoreDefaults
+            )
         self.buttonBox.accepted.connect(self.set_values)
         self.buttonBox.rejected.connect(self.reject)
-        self.buttonBox.button(QDialogButtonBox.RestoreDefaults).\
-            clicked.connect(self.get_defaults)
-
+        self.buttonBox.button(QDialogButtonBox.RestoreDefaults).clicked.connect(self.get_defaults)
         self.layout().addWidget(self.buttonBox, 2, 0)
-
         self.get_values()
 
     def get_defaults(self):
@@ -80,10 +61,8 @@ class PreAnalysisPopup(BasePopup):
         self.gaussian_stdev.field.setValue(self.defaults["gaussian_stdev"])
 
     def set_values(self):
-        self.local_variables["gaussian_stdev"] = \
-            self.gaussian_stdev.field.value()
-        self.local_variables["gauss_x_size"] = \
-            self.gauss_x_size.field.value()
+        self.local_variables["gaussian_stdev"] = self.gaussian_stdev.field.value()
+        self.local_variables["gauss_x_size"] = self.gauss_x_size.field.value()
         self.accept()
 
     def get_values(self):
