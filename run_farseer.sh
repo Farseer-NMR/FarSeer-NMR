a990a3eff707dc0a8554fc96cda09a67a9389633
joaomcteixeira <joaomcteixeira@gmail.com>
2018-03-20 18:13:35 +0100

    removed run script

--- run_farseer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export CONDA_ROOT="$(pwd)/miniconda3/envs/farseernmr"
-export FARSEER_ROOT="$(pwd)"
-export PYTHONPATH=${CONDA_ROOT}:${FARSEER_ROOT}
-
-$CONDA_ROOT/bin/python $FARSEER_ROOT/main.py \$\*
