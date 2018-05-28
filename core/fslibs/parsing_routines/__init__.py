daadf13544be4ff5bd1436dff84b277adad69fc1
Joao MC Teixeira <joaomcteixeira@gmail.com>
2018-05-28 13:18:01 +0200

    added init

---
+++ core/fslibs/parsing_routines/__init__.py
@@ -0,0 +1,7 @@
+import core.fslibs.parsing_routines.ansig.ansig as pr_ansig
+import core.fslibs.parsing_routines.cara_full.cara_full as pr_cara_full
+import core.fslibs.parsing_routines.cara_simple.cara_simple as pr_cara_simple
+import core.fslibs.parsing_routines.ccpnmrv2.ccpnmrv2 as pr_ccpnmrv2
+import core.fslibs.parsing_routines.nmrdraw.nmrdraw as pr_nmrdraw
+import core.fslibs.parsing_routines.nmrview.nmrview as pr_nmrview
+import core.fslibs.parsing_routines.sparky.sparky as pr_sparky
