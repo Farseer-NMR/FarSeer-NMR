bd24c6c4f510b2747061029ae6a0aa69be2e224a
Joao MC Teixeira <joaomcteixeira@gmail.com>
2018-05-28 14:27:41 +0200

    minor correction parsing function

--- core/fslibs/parsing_routines/ccpnmrv2.py
+++ core/fslibs/parsing_routines/ccpnmrv2.py
@@ -23,3 +23,48 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Farseer-NMR. If not, see <http://www.gnu.org/licenses/>.
 """
+def parse_ccpnmrv2_peaklist(peaklist_file):
+    """
+    Parses CCPNMRv2 peaklists into the peakList class format.
+    
+    Parameters:
+        - peaklist_file: path to peaklist file.
+    
+    Returns peakList object
+    """
+    fin = pd.read_csv(peaklist_file)
+    peakList = []
+    atoms = []
+    
+    for row in fin.index:
+        
+        if fin.loc[row,'Assign F1'] in aal1tol3.values():
+            atoms.append(fin.loc[row,'Assign F1'][-1])
+        
+        if fin.loc[row,'Assign F2'] in aal1tol3.values():
+            atoms.append(fin.loc[row,'Assign F2'][-1])
+        
+        peak = Peak(
+            peak_number=fin.loc[row,'Number'],
+            positions=[
+                fin.loc[row,'Position F1'],
+                fin.loc[row,'Position F2']
+                ],
+            atoms=atoms,
+            residue_number=fin.loc[row,'Assign F1'].str[:,-4],
+            residue_type=fin.loc[row,'Assign F1'].str[-4,:],
+            linewidths=[
+                fin.loc[row,'Line Width F1 (Hz)'],
+                fin.loc[row,'Line Width F2 (Hz)']
+                ],
+            volume=fin.loc[row,'Volume'],
+            height=fin.loc[row,'Height'],
+            fit_method=fin.loc[row,'Fit Method'],
+            merit=fin.loc[row,'Merit'],
+            volume_method=fin.loc[row,'Vol. Method'],
+            details=fin.loc[row,'Details'],
+            format='ccpnmrv2'
+            )
+        peakList.append(peak)
+    
+    return peakList
