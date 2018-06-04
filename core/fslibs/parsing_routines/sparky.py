5624042e058f8d8683f01b2d6ab909f94cdcbedc
Joao MC Teixeira <joaomcteixeira@gmail.com>
2018-06-04 12:26:23 +0200

    finished sparky

--- core/fslibs/parsing_routines/sparky.py
+++ core/fslibs/parsing_routines/sparky.py
@@ -27,6 +27,21 @@ import re
 from core.fslibs.Peak import Peak
 
 def parse_sparky_peaklist(peaklist_file):
+    """
+    Parses Sparky peaklists according to format:
+    
+      Assignment         w1         w2       Height       Volume
+     L480HN-L480N      8.772    130.768   1.01E+05   8.27E+05 bx
+     A640HN-A640N      8.657    130.164   5.76E+04   4.94E+05 bx
+     A739HN-A739N      8.846    129.894   8.12E+04   6.77E+05 bx
+     V508HN-V508N      8.650    128.669   1.15E+05   9.24E+05 bx
+     K542HN-K542N      7.369    128.421   1.14E+05   9.43E+05 bx
+    
+    Parameters:
+        - peaklist_file (str): path to peaklist
+    
+    Returns: list of core.fslibs.Peak.Peak objects.
+    """
     peakList = []
     with open(peaklist_file) as f:
         lines = f.readlines()[1:]
@@ -49,7 +64,6 @@ def parse_sparky_peaklist(peaklist_file):
         resname = assignment[0]
         resnumber = assignment[1]
         atoms = [assignment[2].split('-')[0], assignment[-1]]
-        #annotations = [resnumber+aal1tol3[resname]+x[0] for x in atoms]
         linewidths = [None] * 2
         ppms = [line_list[1], line_list[2]]
         height = line_list[3]
