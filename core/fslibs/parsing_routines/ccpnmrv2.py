7e3017071f60620847a7f8405dcbdb6d5fbbccab
Joao MC Teixeira <joaomcteixeira@gmail.com>
2018-06-14 17:19:40 +0200

    added check for ccpnmrv2 peaklist and corrected testing peaklists

--- core/fslibs/parsing_routines/ccpnmrv2.py
+++ core/fslibs/parsing_routines/ccpnmrv2.py
@@ -26,6 +26,7 @@ along with Farseer-NMR. If not, see <http://www.gnu.org/licenses/>.
 import pandas as pd
 from core.utils import aal1tol3
 from core.fslibs.Peak import Peak
+from core.fslibs import wet as fsw
 
 def parse_ccpnmrv2_peaklist(peaklist_file):
     """
@@ -38,6 +39,7 @@ def parse_ccpnmrv2_peaklist(peaklist_file):
     """
     fin = pd.read_csv(peaklist_file)
     peakList = []
+    checks_misleading_chars(fin, peaklist_file)
     
     for row in fin.index:
         atoms = []
@@ -71,3 +73,82 @@ def parse_ccpnmrv2_peaklist(peaklist_file):
         peakList.append(peak)
     
     return peakList
+    
+    
+def checks_misleading_chars(pkl_df, pkl_file_path):
+    """
+    Checks for the presence misleading characters in the DataFrame.
+    This may come from entries of unassigned residues
+    that were not removed.
+    
+    Parameters:
+        - pkl_df (pd.DataFrame): contains converted input peaklist
+        -pkl_file_path (str): path to original peaklist file
+    
+    Returns:
+        - None
+    
+    """
+    # for assignment cols
+    ## empty
+    empty_cells_f1 = pkl_df.loc[:,'Assign F1'].isnull()
+    empty_cells_f2 = pkl_df.loc[:,'Assign F2'].isnull()
+    
+    if empty_cells_f1.values.any() or empty_cells_f2.values.any():
+        rows_bool = empty_cells_f1 | empty_cells_f2
+        msg = "The peaklist {} contains no assignment \
+information in lines {}. Please review that peaklist.".format(
+            pkl_file_path,
+            [2+int(i) for i in rows_bool.index[rows_bool].tolist()]
+            )
+        print(fsw.gen_wet('ERROR', msg, 29))
+        fsw.abort()
+    
+    ## misleading chars
+    non_digit_f1 = \
+        pkl_df.loc[:,'Assign F1'].str.strip().str.contains('\W', regex=True)
+    
+    non_digit_f2 = \
+        pkl_df.loc[:,'Assign F2'].str.strip().str.contains('\W', regex=True)
+    
+    if  non_digit_f1.any() or non_digit_f2.any():
+        rows_bool = non_digit_f1 | non_digit_f2
+        msg = "The peaklist {} contains misleading \
+charaters in Assignment columns in line {}.".format(
+            pkl_file_path,
+            [2+int(i) for i in rows_bool.index[rows_bool].tolist()]
+            )
+        print(fsw.gen_wet('ERROR', msg, 29))
+        fsw.abort()
+    
+    ## for other cols.
+    cols = [
+        'Position F1',
+        'Position F2',
+        'Height',
+        'Volume',
+        'Line Width F1 (Hz)',
+        'Line Width F2 (Hz)',
+        'Merit'
+        ]
+    
+    for col in cols:
+        #if not any(pkl_df.loc[:,col].isnull()):
+            #continue
+        non_digit = pkl_df.loc[:,col].\
+            astype(str).str.strip().str.contains(
+                '[\!\"\#\$\%\&\\\'\(\)\*\,\-\/\:\;\<\=\>\?\@\[\]\^\_\`\{\|\}\~]',
+                regex=True
+                )
+        print(non_digit)
+        if non_digit.any():
+            msg = "The peaklist {} contains misleading \
+charaters in line {} of column [{}].".format(
+                pkl_file_path,
+                [2+int(i) for i in non_digit.index[non_digit].tolist()],
+                col
+                )
+            print(fsw.gen_wet('ERROR', msg, 29))
+            fsw.abort()
+    
+    return
