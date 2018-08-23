22c6976a986ee7de7cf35cf0c8391de4097fc0c7
joaomcteixeira <joaomcteixeira@gmail.com>
2018-08-24 00:29:45 +0200

    improved WET to class type

--- core/fslibs/wet.py
+++ core/fslibs/wet.py
@@ -25,24 +25,70 @@ Functions that format error messages.
 import sys
 import textwrap
 
-def title(s=''):
-    return "    {:@^72}  ".format(" " + s + " ")
+import logging
+import logging.config
+import core.fslibs.log_config as fslogconf
 
-def bottom():
-    return "    {:@^72}  ".format('')
-
-def line(s=''):
-    return "    @{:^70}@  ".format(s)
-
-def referwet(n):
-    return line("+ Refer to WET #{} +".format(n))
-
-def format_msg(string):
-    return "    @{:^70}@  ".format(string)
-
-def gen_wet(t, s, n):
+class WetHandler:
+    """Handles Warning, Errors and Troubleshooting messages"""
     
-    ws = """
+    def __init__(self, msg_type='', msg='', wet_num=0):
+        
+        # initiates log
+        self.logger = fslogconf.getLogger(__name__)
+        logging.config.dictConfig(fslogconf.farseer_log_config)
+        self.logger.debug('logger initiated')
+        
+        if not msg_type:
+            self.logger.debug('WET Message type not provided')
+        
+        if not msg:
+            self.logger.debug('WET Message not provided')
+        
+        if not wet_num:
+            self.logger.debug('WET Number not provided')
+        
+        self.msg_type = msg_type
+        self.msg = msg
+        self.wet_num = wet_num
+        self.logger.debug('Type, MSG and NUM configured correctly')
+        
+        self.generate_wet()
+    
+    def _title(self, msg_type=''):
+        return "    {:@^72}  ".format(" " + msg_type + " ")
+    
+    def _bottom(self):
+        return "    {:@^72}  ".format('')
+
+    def _line(self, s=''):
+        return "    @{:^70}@  ".format(s)
+
+    def referwet(self, n):
+        return line("+ Refer to WET #{} +".format(n))
+
+    def _format_msg(self, s):
+        return "    @{:^70}@  ".format(s)
+
+    def generate_wet(self, title='', msg='', wet_num=0):
+        """
+        Generates wet message box. Message type, message and 
+        WET number are optional, default to class instantiation.
+        
+        Parameters:
+            - title (str): the message type: ERROR, Trouble, WARNING, ...
+            - msg (str): the WET full message
+            . wet_num (int): the documentation index WET number of the message
+        
+        Returns:
+            - None. Assigns self.wet
+        """
+    
+        t = title or self.msg_title
+        msg = msg or self.msg
+        wet_num = wet_num or self.wet_num
+        
+        self.wet = """
 {}
 {}
 {}
@@ -52,40 +98,55 @@ def gen_wet(t, s, n):
 {}
 {}
 
-""".\
-        format(
-            title(t),
-            line(),
-             "\n".join(map(format_msg, textwrap.wrap(s, width=67))),
-            line(),
-            referwet(n),
-            line(s="please visit"),
-            line(
-                s="github.com/joaomcteixeira/FarSeer-NMR/wiki/WET-List#wet{}".\
-                    format(n)
-                ),
-            bottom()
-            )
+    """.\
+            format(
+                _title(t),
+                _line(),
+                 "\n".join(map(_format_msg, textwrap.wrap(msg, width=67))),
+                _line(),
+                referwet(n),
+                _line(s="please visit"),
+                _line(
+                    s="github.com/joaomcteixeira/FarSeer-NMR/wiki/WET-List#wet{}".\
+                        format(n)
+                    ),
+                _bottom()
+                )
+        
+        self.logger.debug('WET Msg generated correctly')
+        
+        return None
     
-    return ws
+    def print_wet(self):
+        """Prints WET message"""
+        print(self.wet)
+        return None
 
-def continue_abort():
-    """ Asks user to decide behaviour. """
-    choice = 'z'
+    def continue_abort(self, choice=z):
+        """
+        Asks user to decide behaviour. A predifined choice can be given.
+        
+        Parameters:
+            - choice (str): ['A', 'C']. 
+        """
     
-    while not(choice in ['A', 'C']):
-        choice = \
-            input('> What do you want to do? [C]ontinue or [A]bort? ').upper()
-    
-    if choice == 'A':
-        abort(abort_msg)
-    
-    elif choice == 'C':
-        return 'Continuing...'
+        while not(choice in ['A', 'C']):
+            self.logger.debug('Entered while cycle correctly')
+            choice = \
+                input('> What do you want to do? [C]ontinue or [A]bort? ').\
+                    upper()
+        
+        self.logger.debug('Choice selected "{}"'.format(choice))
+        
+        if choice == 'A':
+            self.abort(abort_msg)
+        
+        elif choice == 'C':
+            return 'Continuing...'
 
-def abort(m=''):
-    sys.exit(m)
-    return
+    def abort(self, m=''):
+        sys.exit(m)
+        return
 
 def end_good():
     s = \
