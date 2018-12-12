2d640180e3866ddaa7af93825a7fdeab8221836e
joaomcteixeira <joaomcteixeira@gmail.com>
2018-12-12 13:28:54 +0100

    removed test_farseer.sh

--- test_farseer.sh
+++
@@ -1,6 +0,0 @@
-export FARSEER_ROOT="$(pwd)"
-export PYTHONPATH=$FARSEER_ROOT
-
-nosetests -v -w $FARSEER_ROOT/core/testing
-nosetests -v -w $FARSEER_ROOT/gui/tabs/testing
-nosetests -v -w $FARSEER_ROOT/gui/popups/testing
